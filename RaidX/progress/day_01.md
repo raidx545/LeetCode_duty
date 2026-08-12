e003647c887f1412d0849114dca0baca0a6137fa
Raj Porwl <raazporwal6@gmail.com>
2026-08-13 00:43:20 +0530

    Day_1

---
+++ RaidX/progress/day_01.md
@@ -0,0 +1,3 @@
+Today i solved only 1 problem
+Problem based on HashMap, Word Pattern
+Main thing is - Bijective Mapping therefore i used 2 HashMaps
---
+++ RaidX/solutions/290_Word Pattern/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if(words.length != pattern.length()) return false ;
+        HashMap<Character , String> map1 = new HashMap<>();
+        HashMap<String,Character> map2 = new HashMap<>();
+        for(int i = 0 ;i < words.length ; i++){
+            if( !(map1.containsKey(pattern.charAt(i))) && !(map2.containsKey(words[i]) )  ){
+                map1.put(pattern.charAt(i),words[i]);
+                map2.put(words[i],pattern.charAt(i));
+            }
+
+            else if( ((map1.containsKey(pattern.charAt(i))) && !(map2.containsKey(words[i])) || !((map1.containsKey(pattern.charAt(i))) && (map2.containsKey(words[i]))) ) ) return false ;
+            
+            else if( !map1.get(pattern.charAt(i)).equals(words[i])  || map2.get(words[i]) != pattern.charAt(i)) return false ;
+        }
+        return true ;
+    }
+}

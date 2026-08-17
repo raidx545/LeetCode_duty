a86926b6c4bf4226ce178acfaa5ace8a83fbb4f9
Raj Porwl <raazporwal6@gmail.com>
2026-08-18 01:38:27 +0530

    Day_05

---
+++ RaidX/progress/day_05.md
@@ -0,0 +1,4 @@
+Today i solve 1 problem - 
+    1. LeetCode 7 Reverse the Integer 
+Initially i mixed up two concepts, reversing the bits and reverse the integer
+It asked to reverse the 32 bit signed integer, if reversed number is out of range of Integer then return 0 otherwise return the reversed Integer
---
+++ RaidX/solutions/7_Reverse_Integer/Solution.java
@@ -0,0 +1,24 @@
+package solutions.7_Reverse_Integer;
+
+class Solution {
+    public int reverse(int x) {
+        int reverse = 0;
+        int n = Math.abs(x);
+        if(x == Integer.MIN_VALUE) return 0 ;
+        while (n != 0) {
+            int lastInt = n % 10;
+
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                (reverse == Integer.MAX_VALUE / 10 && lastInt > 7))
+                return 0;
+
+            reverse = reverse * 10 + lastInt;
+            n = n / 10;
+        }
+
+        if (x < 0)
+            return reverse * (-1);
+
+        return reverse;
+    }
+}
